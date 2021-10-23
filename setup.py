0828b7c53dc99874844db40bad7c68d7a564c0c6
Marcus Evans <marcus@marcusbevans.com>
2021-10-22 23:56:10 -0600

    renaming to avoid conflicts with other python filemaker packages

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description='Python library for FileMaker JDBC/ODBC Driver',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/caeneb/cnfilemaker.git',
+    url='https://github.com/caeneb/cnfilemaker',
     project_urls={
         "Bug Tracker": "https://github.com/caeneb/cnfilemaker/issues"
     },
