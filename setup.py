e4fa2f105becc85390155d61b2b08265e0d8f2a3
Marcus Evans <marcus@marcusbevans.com>
2021-10-22 23:54:56 -0600

    renaming to avoid conflicts with other python filemaker packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='cnfilemaker',
-    version='0.0.1.6',
+    version='0.0.1.7',
     author='Marcus Evans',
     author_email='marcus@marcusbevans.com',
     description='Python library for FileMaker JDBC/ODBC Driver',
